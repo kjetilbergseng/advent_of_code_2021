d43ff9a6625a48bc9507605baa51c1e922c0d2c6
Kjetil Bergseng <kjetil.bergseng@gmail.com>
2021-12-01 17:56:48 +0100

    fixed the test

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,20 +1,11 @@
 module Day1 where
-
---day1
-f a b
-  | b > a = 1
-  | otherwise = 0
-
 sum3 a b c = a + b + c
-
-day1a li = sum . zipWith f li $ tail li
-
+day1a li = length . filter id $ zipWith (<) li (tail li)
 day1b li = zipWith3 sum3 li (tail li) (drop 2 li)
-
 toInt x = read x :: Int
-
 day1 = do
   putStrLn "day1"
   contents <- readFile "../input/day1.txt"
-  print . day1a $ map toInt (lines contents)
-  print . day1a . day1b $ map toInt (lines contents)
+  let input = map toInt (lines contents)
+  print . day1a $ input
+  print . day1a . day1b $ input
--- test/Spec.hs
+++ test/Spec.hs
@@ -4,5 +4,5 @@ main :: IO ()
 main = do
   putStrLn ""
   putStrLn $ if day1a [199, 200, 208, 210, 200, 207, 240, 269, 260, 263] == 7 then "Day 1a OK" else "Failed day1a!"
-  putStrLn $ if day1a . day1b [199, 200, 208, 210, 200, 207, 240, 269, 260, 263] == 5 then "Day 1b OK" else "Failed day1b!"
+  putStrLn $ if (day1a . day1b $ [199, 200, 208, 210, 200, 207, 240, 269, 260, 263]) == 5 then "Day 1b OK" else "Failed day1b!"
   return ()
