8b8bd51f6bcf842a0c7d0b0cfafcbf5e27d3429c
Kjetil Bergseng <Kjetil.Bergseng@dnvgl.com>
2021-12-01 15:39:37 +0100

    added tests for day1

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,6 @@ import Day1
 main :: IO ()
 main = do
   putStrLn ""
-  putStrLn $ if day1a [1721, 979, 1010, 366, 299, 675, 1456] 2020 == 514579 then "Day 1a OK" else "Failed day1a!"
-  putStrLn $ if day1b [1721, 979, 1010, 366, 299, 675, 1456] 2020 == 241861950 then "Day 1b OK" else "Failed day1b!"
+  putStrLn $ if day1a [199, 200, 208, 210, 200, 207, 240, 269, 260, 263] == 7 then "Day 1a OK" else "Failed day1a!"
+  putStrLn $ if day1a . day1b [199, 200, 208, 210, 200, 207, 240, 269, 260, 263] == 5 then "Day 1b OK" else "Failed day1b!"
   return ()
