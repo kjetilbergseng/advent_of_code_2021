9de31028cd77e2b12cf812cd09c2063bac6271b9
Kjetil Bergseng <Kjetil.Bergseng@dnvgl.com>
2021-12-02 16:10:16 +0100

    corrected spelling

--- src/Day2.hs
+++ src/Day2.hs
@@ -5,13 +5,13 @@ import Data.Function ((&))
 
 toInt x = read x :: Int
 
-postionChange (str, int)
+positionChange (str, int)
   | str == "forward" = (toInt int, 0)
   | str == "up" = (0, - toInt int)
   | str == "down" = (0, toInt int)
   | otherwise = (0, 0)
 
-tplSum (a, b) (c, d) = (a + c, b + d)
+pairSum (a, b) (c, d) = (a + c, b + d)
 
 aim (a, b, c) (d, e, _) = (a + d, b + d * c, c + e)
 
@@ -21,11 +21,11 @@ tripleToPair (a, b, _) = (a, b)
 
 mapReduce transform reduce init li = foldl reduce init (map transform li)
 
-day2a li = uncurry (*) (mapReduce postionChange tplSum (0, 0) li)
+day2a li = uncurry (*) (mapReduce positionChange pairSum (0, 0) li)
 
 day2b li =
   li
-    & mapReduce (pairToTriple . postionChange) aim (0, 0, 0)
+    & mapReduce (pairToTriple . positionChange) aim (0, 0, 0)
     & tripleToPair
     & uncurry (*)
 
