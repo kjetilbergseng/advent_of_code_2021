0a1d1393e8172ac5e4e84839269711c07c211e9a
Kjetil Bergseng <kjetil.bergseng@gmail.com>
2021-12-06 22:19:17 +0100

    day6 update

--- src/Day6.hs
+++ src/Day6.hs
@@ -2,9 +2,9 @@ module Day6 where
 import UtilityFunctions
 import Control.Monad (join)
 
-countFish day days list
-    | day==days = list
-    | otherwise = countFish (day+1) days (zipWith (+) (rotate 1 list) [0,0,0,0,0,0,head list,0,0])
+countFish days list
+    | days==0 = list
+    | otherwise = countFish (days-1) (zipWith (+) (rotate 1 list) [0,0,0,0,0,0,head list,0,0])
 numberInBin n = length . filter (==n) 
 createBins n input = [ numberInBin x input | x <- [0..n]]
 day6 = do
@@ -13,5 +13,5 @@ day6 = do
   let input = map readInteger (split ',' contents)
   let bins=createBins 8 input
 
-  print . sum $ countFish 0 80 bins
-  print . sum $ countFish 0 256 bins
+  print . sum $ countFish 80 bins
+  print . sum $ countFish 256 bins
