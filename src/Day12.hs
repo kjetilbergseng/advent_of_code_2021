950fea5a6a9a1812211529d9ec17babdb860b893
Kjetil Bergseng <kjetil.bergseng@gmail.com>
2021-12-13 17:41:56 +0100

    day12

--- src/Day12.hs
+++ src/Day12.hs
@@ -14,17 +14,17 @@ solve visited (li, usedSmallRoomVisit) current
         (join $ map (\\[current]) (filter (elem current) li))
 
 updateList li usedSmallRoomVisit current visited
-        | current == "start" = (rs, usedSmallRoomVisit)
-        | isUpper (head current) = (rs, usedSmallRoomVisit)
-        | current `elem` visited || usedSmallRoomVisit= (filteredList, True)
-        | otherwise = (rs, usedSmallRoomVisit)
-    where 
-        rs = filter (notElem "start") li
-        filteredList=filterOutVisitedSmalRooms (current:visited) li
+    | current == "start" = (rs, usedSmallRoomVisit)
+    | any isUpper current = (rs, usedSmallRoomVisit)
+    | current `elem` visited || usedSmallRoomVisit= (filteredList, True)
+    | otherwise = (rs, usedSmallRoomVisit)
+  where 
+    rs = filter (notElem "start") li
+    filteredList=filterOutVisitedSmalRooms (current:visited) li
 
 filterOutVisitedSmalRooms [] li = li
 filterOutVisitedSmalRooms (v:vs) li
-    | isUpper (head v) = filterOutVisitedSmalRooms vs li
+    | any isUpper v = filterOutVisitedSmalRooms vs li
     | otherwise = filterOutVisitedSmalRooms vs (filter (notElem v) li)
     
 day12 = do
