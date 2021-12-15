63ebb6ab4c57bd0361e73bf61c25d9af0181584d
Kjetil Bergseng <Kjetil.Bergseng@dnvgl.com>
2021-12-15 16:43:57 +0100

    day14 refactor

--- src/Day14.hs
+++ src/Day14.hs
@@ -5,37 +5,37 @@ import Data.Maybe (fromMaybe)
 
 getVal key map = fromMaybe 0 (Map.lookup key map)
 
-getRule key map = fromMaybe "" (Map.lookup key map)
+getTransformations key map = fromMaybe "" (Map.lookup key map)
 
-update [] rules map mapOut = mapOut
-update (key : keys) rules map mapOut
-  | getVal key map == 0 = update keys rules map mapOut
-  | otherwise = update keys rules map (addToMap (getRule key rules) (getVal key map) mapOut)
+update [] transformations mapIn mapOut = mapOut
+update (key : keys) transformations mapIn mapOut
+  | getVal key mapIn == 0 = update keys transformations mapIn mapOut
+  | otherwise = update keys transformations mapIn (addToMap (getTransformations key transformations) (getVal key mapIn) mapOut)
 
 changPos s = head s : s !! 2 : [s !! 1]
 
-addToMap template val mapOut
-  | length template < 2 = mapOut
-  | otherwise = addToMap (drop 1 template) val (Map.insertWith (+) (take 2 template) val mapOut)
+addToMap input val mapOut
+  | length input < 2 = mapOut
+  | otherwise = addToMap (drop 1 input) val (Map.insertWith (+) (take 2 input) val mapOut)
 
-performSteps step steps keys rules mapOut
+performSteps step steps keys transformations mapOut
   | step == steps = mapOut
-  | otherwise = performSteps (step + 1) steps keys rules (update keys rules mapOut Map.empty)
+  | otherwise = performSteps (step + 1) steps keys transformations (update keys transformations mapOut Map.empty)
 
-solve hashMap keys template = (-) <$> maximum <*> minimum $ [getVal key sumMap | key <- Map.keys sumMap]
+solve hashMap keys input = (-) <$> maximum <*> minimum $ [getVal key sumMap | key <- Map.keys sumMap]
   where
-    startMap = Map.insertWith (+) [last template] 1 Map.empty
+    startMap = Map.singleton [last input] 1
     sumMap = foldl (\m k -> Map.insertWith (+) (take 1 k) (getVal k hashMap) m) startMap keys
 
 day14 = do
   putStrLn "day14"
   contents <- readFile "../input/day14.txt"
-  let template = head $ lines contents
-  let rules = map (changPos . filter (\x -> x /= ' ' && x /= '-' && x /= '>')) (drop 2 $ lines contents)
+  let input = head $ lines contents
   let keys = map (takeWhile (/= ' ')) (drop 2 $ lines contents)
+  let values = map (changPos . filter (\x -> x /= ' ' && x /= '-' && x /= '>')) (drop 2 $ lines contents)
 
-  let ruleMap = Map.fromList (zip keys rules)
-  let startMap = addToMap template 1 Map.empty
+  let transformationMap = Map.fromList (zip keys values)
+  let startMap = addToMap input 1 Map.empty
 
-  print $ solve (performSteps 0 10 keys ruleMap startMap) keys template
-  print $ solve (performSteps 0 40 keys ruleMap startMap) keys template
+  print $ solve (performSteps 0 10 keys transformationMap startMap) keys input
+  print $ solve (performSteps 0 40 keys transformationMap startMap) keys input
